5c17e5605ef09b8812bdf09b6fa0cda9a55dc412
Panagiotis Chronopoulos <panoschrono@gmail.com>
2024-09-02 17:58:04 +0300

    KG now includes original text

--- dataset_simplification/simplify_dataset.py
+++ dataset_simplification/simplify_dataset.py
@@ -11,9 +11,9 @@ api_key = get_key('../utils/api_key.txt')
 client  = openai.OpenAI(api_key=api_key)
 
 input_file  = sys.argv[1]
-OUTPUT      = 'simplified_dataset.txt'
+OUTPUT      = 'simplified_dataset.csv'
 
 with open(input_file, 'r') as file, open(OUTPUT, 'w') as output_file:
     for line in tqdm(file, desc= 'Simplifying Dataset'):
-        output_file.write(simplify_text(line, client) + '\n')
+        output_file.write(line.strip() + '|' + simplify_text(line, client) + '\n')
         pass
--- graph_creation/create_KG_dataset.py
+++ graph_creation/create_KG_dataset.py
@@ -3,16 +3,18 @@ import sys
 from utils.utils import create_graph
 from tqdm import tqdm
 import json
+import csv
 
 TARGET = 'KG_dataset.jsonl'
 
 nlp = spacy.load("en_core_web_trf")
 
 with open(sys.argv[1], 'r') as file, open(TARGET, 'w') as output:
-    for i, line in enumerate(tqdm(file, desc='Creating KG Dataset')):
-        KG_json = create_graph(line, nlp)
+    reader = csv.reader(file, delimiter='|')
+    for i, row in enumerate(tqdm(reader, desc='Creating KG Dataset')):
+        KG_json = create_graph(row[1], nlp)
 
-        json_data = {"doc_id":  i+1, "text": line}
+        json_data = {"doc_id":  i+1, "text": row[0]}
         json_data.update(KG_json)
 
         json_line = json.dumps(json_data)
