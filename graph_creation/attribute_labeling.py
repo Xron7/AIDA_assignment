162bdecf5f1ba883dadf33d2d6c90545a4a2c9d3
Panagiotis Chronopoulos <panoschrono@gmail.com>
2024-09-03 21:57:32 +0300

    Added sleep to the labeling code, to reduce usage per minute

--- graph_creation/attribute_labeling.py
+++ graph_creation/attribute_labeling.py
@@ -3,6 +3,7 @@ import sys
 from utils.utils import characterize_attributes, get_key
 import openai
 from tqdm import tqdm
+import time
 
 #
 # argument is the jsonl file
@@ -16,6 +17,7 @@ client  = openai.OpenAI(api_key=api_key)
 with open(sys.argv[1], 'r') as file, open(TARGET, 'w') as output:
     for i, json_data in enumerate(tqdm(file, desc='Characterizing Attributes and Objects')):
         json_data_attr = characterize_attributes(json.loads(json_data.strip()), client)
+        time.sleep(10)
 
         json_line = json.dumps(json_data_attr)
         output.write(json_line + '\n')
